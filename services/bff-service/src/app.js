2cd30eafd23a723618b74b2ac1ae00fc491000da
Begum <110823067+begum-e@users.noreply.github.com>
2022-11-13 19:54:08 +0300

    Node.js Backend for frontend (bff-service) implemented by using Express, caching added, elasticbeanstalk integration

--- services/bff-service/src/app.js
+++ services/bff-service/src/app.js
@@ -1,70 +1,69 @@
-const express = require('express');
-const fetch = require('node-fetch');
-require('dotenv').config();
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
-const axios = require('axios');
+const axios = require("axios");
 
-/*const nodecache = require('node-cache');
-const appCache = new nodecache({ stdTTL: 120 });*/
+const config = require("./config");
+
+const nodecache = require("node-cache");
+const cache = new nodecache({ stdTTL: 120 });
 
 app.use(express.json());
 
-let cache = {};
-const cacheResponse = (status, data) => {
-	cache = { status, data };
-	setTimeout(() => {
-		cache = {};
-	}, 120000);
-};
+app.all("/*", async (request, response) => {
+  const { method, path, originalUrl, body, headers } = request;
+  console.log("originalUrl->", originalUrl);
 
-app.all('/*', async (request, res) => {
-	const { method, path, originalUrl, body, headers } = request;
-	console.log('originalUrl->', originalUrl);
+  const recipient = path.split("/")[1];
+  const target = config[recipient];
+  console.log("target->", target);
 
-	const recipient = path.split('/')[1];
-	const target = process.env[recipient];
-	console.log('target->', target);
+  if (!target) {
+    response.status(502).send({ message: "Cannot process the request" });
+    return;
+  }
 
-	if (!target) {
-		res.status(502).send({ message: 'Cannot process the request' });
-		return;
-	}
+  try {
+    const axiosConfig = {
+      method: method,
+      url: `${target}${originalUrl}`,
+      headers: {
+        Authorization: headers.authorization,
+      },
+      ...(Object.keys(body || {}).length > 0 && { data: body }),
+    };
 
-	/*const shouldUseCache = path === '/products' && method.toUpperCase() === 'GET';
-
-	if (shouldUseCache && cache.data) {
-		res.status(cache.status).send(cache.data);
-		return;
-	}*/
-
-	try {
-		const axiosConfig = {
-			method: method,
-			url: `${target}${originalUrl}`,
-			headers: {
-				Authorization: headers.authorization,
-			},
-			...(Object.keys(body || {}).length > 0 && { data: body }),
-		};
-
-		axios
-			.request(axiosConfig)
-			.then((response) => res.json(response.data))
-			.catch((error) => {
-				if (error.response) {
-					const { status, data } = error.response;
-
-					res.status(status).json(data);
-				} else {
-					res.status(500).json({ error: error.message });
-				}
-			});
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
+    axios
+      .request(axiosConfig)
+      .then((resp) => {
+        if (method === "GET") {
+          // get from cache if exist
+          if (cache?.has(recipient)) {
+            console.log("Getting from cache ->", recipient);
+            return response.json(cache.get(recipient));
+          }
+          // put to cache
+          console.log("Putting to cache ->", recipient);
+          cache.set(recipient, resp.data);
+          response.json(resp.data);
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          const { status, data } = error.response;
+          response.status(status).json(data);
+        } else {
+          response.status(500).json({ error: error.message });
+        }
+      });
+  } catch (error) {
+    response.status(500).send({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
-	console.log(`Backend For Frontend App listening on port ${port}`);
+  console.log(`Backend For Frontend App listening on port ${port}`);
 });
